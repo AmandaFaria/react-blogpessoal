8b6ef69bdf018cc6db8cb32b22cd5e38a5bf63f3
AmandaFaria <amanda34faria@gmail.com>
2021-12-03 19:56:17 -0300

    Criando a tela de cadastro usuário

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,35 @@ import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import './App.css';
+
 
 
 function App() {
   return (
     <Router>
       <Navbar />
-        <Switch>
-          <div style={{minHeight: '100vh'}}>
+      <Switch>
+        <div style={{ minHeight: '100vh' }}>
 
-            <Route path='/login'>
-              <Login />
-            </Route>
+          <Route exact path='/'>
+            <Login />
+          </Route>
 
-            <Route path='/home'>
-              <Home />
-            </Route>
-          </div>
-        </Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+
+          <Route path='/home'>
+            <Home />
+          </Route>
+
+          <Route path='/cadastrousuario'>
+            <CadastroUsuario />
+          </Route>
+        </div>
+      </Switch>
       <Footer />
     </Router>
   );
